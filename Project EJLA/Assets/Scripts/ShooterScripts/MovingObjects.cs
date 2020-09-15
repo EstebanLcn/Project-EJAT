01f2728daa991559dffcc0951c480c1a011b569a
EstebanLcn <44468643+EstebanLcn@users.noreply.github.com>
2020-09-16 00:10:39 +0200

    Problem about infinite scrolling...

---
+++ Project EJLA/Assets/Scripts/ShooterScripts/MovingObjects.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObjects : MonoBehaviour
+{
+    public float movespeed;
+   
+       // Update is called once per frame
+    void Update()
+    {
+        transform.position -= new Vector3(movespeed * Time.deltaTime, 0f, 0f);
+    }
+    private void OnBecameInvisible() {
+        Destroy(gameObject);
+    }
+}
