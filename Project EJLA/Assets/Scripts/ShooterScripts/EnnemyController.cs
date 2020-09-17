2af288935b0e982b167292f5d9710139bd983f43
EstebanLcn <44468643+EstebanLcn@users.noreply.github.com>
2020-09-18 01:39:12 +0200

    HealthSystem + Ennemies

---
+++ Project EJLA/Assets/Scripts/ShooterScripts/EnnemyController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyController : MonoBehaviour
+{
+        public float movespeed;
+        public Vector2 startDirection;
+        public bool shouldChangeDirection;
+        public float changeDirectionXPoint;
+        public Vector2 ChangedDirection;
+        public GameObject shot;
+        public Transform firePoint;
+         public float timeBetweenShots = .1f;
+         public bool canShot;
+         public bool allowShoot;
+    private float ShotsCounter; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShotsCounter = timeBetweenShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // transform.position -= new Vector3(movespeed * Time.deltaTime, 0f, 0f);
+       if(!shouldChangeDirection)
+       {
+                  transform.position += new Vector3(startDirection.x * movespeed * Time.deltaTime, startDirection.y * movespeed * Time.deltaTime, 0f);
+
+       }else
+       if(transform.position.x > changeDirectionXPoint)
+       {
+                             transform.position += new Vector3(startDirection.x * movespeed * Time.deltaTime, startDirection.y * movespeed * Time.deltaTime, 0f);
+
+       }else {
+                             transform.position += new Vector3(ChangedDirection.x * movespeed * Time.deltaTime, ChangedDirection.y * movespeed * Time.deltaTime, 0f);
+
+       }
+       if(allowShoot)
+       {
+       ShotsCounter -= Time.deltaTime;
+                if(ShotsCounter <=0)
+                {
+                     ShotsCounter = timeBetweenShots;
+                  Instantiate(shot, firePoint.position, firePoint.rotation);
+                 
+                }
+       }
+
+
+    }
+    private void OnBecameInvisible() {
+        Destroy(gameObject);
+    }
+    private void OnBecameVisible() {
+        if(canShot){
+            allowShoot = true;
+        }
+    }
+}
---
+++ Project EJLA/Assets/Scripts/ShooterScripts/HealthManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+         public static HealthManager instance;
+
+    public int currentHealth, maxHealth;
+
+    public GameObject deathEffect;
+    // Start is called before the first frame update
+ private void Awake()
+  {
+        instance = this;
+    }
+        void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void HurtPlayer()
+    {
+        currentHealth--;
+        if(currentHealth <=0)
+        {
+            Instantiate(deathEffect, transform.position, transform.rotation);
+            gameObject.SetActive(false);
+        }
+    }
+}
